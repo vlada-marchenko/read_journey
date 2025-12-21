512a7d90e267b389ef62d1cc862ffe0d9d83b2f9
vlada-marchenko <vvvvadya44@gmail.com>
2025-12-21 03:35:51 +0100

    add header mobile

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import { Login } from '../../pages/Login/Login.tsx'
 import { Registration } from '../../pages/Registration/Registration.tsx'
 import ProtectedRoute from '../../auth/ProtectedRoute'
@@ -8,6 +8,16 @@ import PublicRoute from '../../auth/PublicRoute'
 import Library from '../../pages/Library/Library.tsx'
 import Recommended from '../../pages/Recommended/Recomended'
 import Reading from '../../pages/Reading/Reading.tsx'
+import Header from '../Header/Header.tsx'
+
+function AppLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
 return (
@@ -15,10 +25,12 @@ return (
     <Route path="/register" element={<PublicRoute><Registration /></PublicRoute>} />
     <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} /> 
 
-    <Route path='/' element={<ProtectedRoute><Recommended /></ProtectedRoute>} />
-    <Route path='/library' element={<ProtectedRoute><Library /></ProtectedRoute>} />
-    <Route path='/recommended' element={<ProtectedRoute><Recommended /></ProtectedRoute>} />
-    <Route path='/reading/:bookId' element={<ProtectedRoute><Reading /></ProtectedRoute>} />
+    <Route element={<AppLayout />}>
+      <Route path='/' element={<ProtectedRoute><Recommended /></ProtectedRoute>} />
+      <Route path='/library' element={<ProtectedRoute><Library /></ProtectedRoute>} />
+      <Route path='/recommended' element={<ProtectedRoute><Recommended /></ProtectedRoute>} />
+      <Route path='/reading/:bookId' element={<ProtectedRoute><Reading /></ProtectedRoute>} />
+    </Route>
   </Routes>
 )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,58 @@
 import css from './Header.module.css'
+import Icon from '../Icon/Icon'
+import { useAuth } from '../../auth/AuthContext';
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router';
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+        const closeMenu = () => {
+        setMenuOpen(false);
+    }
+
+        useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "auto"
+    }, [menuOpen])
+
+    const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+   if (e.target instanceof HTMLElement && e.target.classList.contains(css.overlay)) {
+    setMenuOpen(false);
+  }
+  };
+
+  const { user } = useAuth();
+
+  const userName = user?.name ?? "";
+
     return (
         <div className={css.container}>
-
+            <div className={css.window}>
+            <Icon name="logo-mob" width={42} height={17}/>
+            <div className={css.right}>
+                <div className={css.circle}>
+                    {userName.charAt(0).toUpperCase()}
+                </div>
+                <button className={css.menuButton} onClick={() => setMenuOpen(!menuOpen)} aria-label="Menu">
+                    <Icon name="menu" width={28} height={28}/>
+                </button>
+            </div>
+            {menuOpen && (
+                <div className={css.overlay} onClick={handleOverlayClick}>
+                    <nav className={css.menu}>
+                        <button className={css.closeButton} onClick={closeMenu}>
+                            <Icon name="close" width={28} height={28}/>
+                        </button>
+                        <NavLink to='/recommended' onClick={closeMenu}   className={({ isActive }) =>
+    isActive ? `${css.link} ${css.active}` : css.link
+  }>Home</NavLink>
+                        <NavLink to='/library' onClick={closeMenu}   className={({ isActive }) =>
+    isActive ? `${css.link} ${css.active}` : css.link
+  }>My library</NavLink>
+                    </nav>
+                </div>
+            )}
+            </div>
         </div>
     )
 }
